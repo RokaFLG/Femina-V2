ba656d1415d00c214e3eabdafb725727881202fb
unknown <you@example.com>
2022-07-11 21:05:51 -0300

    Cambios enormes en el proyecto

--- app.js
+++ app.js
@@ -5,43 +5,43 @@ let texto=" "
 while (entrada!= "ESC" && entrada != "esc") {
     texto +=entrada + " "
     entrada= prompt("Intrese texto o ESC para finalizar")
-}
+  }
 alert(texto)
 for (let pio=1;pio<=10;pio++){
-    if(pio===5){
-        continue;
-    }
-    alert(`El valor de pio es ${pio}`);
-}*/
-// 2do Desafio
-/*function credito(final, cuotas){
-    switch(cuotas){
-        case "1":
-            console.log("Ha seleccionado la opcion de 2 cuotas")
-         case "2":
-            console.log("Ha seleccionado la opcion de 4 cuotas")
-            final = final * 1.1; 
-            break;
-             case "3":
-                console.log("Ha seleccionado la opcion de 6 cuotas")
-              case "4":
-                console.log("Ha seleccionado la opcion de 12 cuotas")
-                final = final * 1.2;
-                break;
-                case "5":
-                    console.log("Ha seleccionado la opcion de 24 cuotas")
-                case "6":
-                    console.log("Ha seleccionado la opcion de 36 cuotas")
-                    final = final *1.4;
-                    break;
-                    default: 
-                    alert("Cuotas no validas")
-    }
-    return final;
+  if(pio===5){
+    continue;
+  }
+  alert(`El valor de pio es ${pio}`);
 }
-let cuota = prompt(
+// 2do Desafio
+function credito(final, cuotas){
+  switch(cuotas){
+    case "1":
+            console.log("Ha seleccionado la opcion de 2 cuotas")
+            case "2":
+              console.log("Ha seleccionado la opcion de 4 cuotas")
+              final = final * 1.1; 
+              break;
+             case "3":
+               console.log("Ha seleccionado la opcion de 6 cuotas")
+               case "4":
+                 console.log("Ha seleccionado la opcion de 12 cuotas")
+                 final = final * 1.2;
+                 break;
+                 case "5":
+                   console.log("Ha seleccionado la opcion de 24 cuotas")
+                   case "6":
+                      console.log("Ha seleccionado la opcion de 36 cuotas")
+                      final = final *1.4;
+                      break;
+                      default: 
+                      alert("Cuotas no validas")
+                    }
+                    return final;
+                  }
+                  let cuota = prompt(
     `Porfavor seleccione una cantidad de cuotas:
-1: 2 Cuotas
+    1: 2 Cuotas
 2: 4 Cuotas
 3: 6 Cuotas
 4: 12 Cuotas
@@ -50,7 +50,6 @@ let cuota = prompt(
 )
 const total = credito (15000, cuota);
 alert(`Su total a pagar es de ${total.toFixed(2)}pesos`);
-
 */
 let arrayProductos=[];
 let arrayCarrito =[];
@@ -71,62 +70,49 @@ class Carrito{
   }
 }
 let j=0;
-arrayProductos[j++]= new Producto("Chanchito",1000,9);
-arrayProductos[j++]= new Producto("Elefante",800,12);
-arrayProductos[j++]= new Producto("Lobo",1200,5);
+arrayProductos[j++]= new Producto("Taza Magica",1200,15);
+arrayProductos[j++]= new Producto("Taza con dibujo",850,25);
+arrayProductos[j++]= new Producto("Taza con Foto",700,35);
 
 
-function seleccionCarrito(peluche) {
-  switch (peluche) {
+function seleccionCarrito(taza) {
+  switch (taza) {
     case 0:
-      if (arrayProductos[peluche].stock < 1) {
-        alert("no mas Chanchitos no disponible, stocks nuevos cada semana o comunicarse directametne para mayor rapides");  
+      if (arrayProductos[taza].stock < 1) {
+        alert("Se ha agotado el producto de Taza Magica, Si tiene dudas o consultas comuniquese con el centro de atencion al cliente");  
       } else {
         
-        agregarCarrito(arrayProductos[peluche],arrayCarrito);
+        agregarCarrito(arrayProductos[taza],arrayCarrito);
 
-        alert("Chanchito agregado al carrito");
-        console.log("Elefante agregado al carrito");
+        alert("Taza Magica ha sido comprada");
+        console.log("Taza Magica ha sido comprada");
       }
       console.log(arrayCarrito);
-  
-      /*
-      a = new Carrito (arrayProductos[peluche].nombre,arrayProductos[peluche].precio,arrayProductos[peluche].cantidad+1);
-      flag++;
-      if (a.flag) {        
-        arrayCarrito.push(a);
-      } else {        
-      }
-      arrayCarrito.push(arrayProductos[peluche]);
-      console.log(arrayProductos[peluche].precio); 
-      arrayProductos[peluche].stock--;   
-      console.log("----------------------");     
-      console.log(arrayCarrito.length);
-      */ 
+
       break;
     case 1:
-      if (arrayProductos[peluche].stock < 1) {
-        alert("no mas Elefantes no disponible, stocks nuevos cada semana o comunicarse directametne para mayor rapides");  
+      if (arrayProductos[taza].stock < 1) {
+        alert("Se ha agotado el producto de Taza con Dibujo, Si tiene dudas o consultas comuniquese con el centro de atencion al cliente");  
       } else {
         
-        agregarCarrito(arrayProductos[peluche],arrayCarrito);
+        agregarCarrito(arrayProductos[taza],arrayCarrito);
         
-        alert("Elefante agregado al carrito");
-        console.log("Elefante agregado al carrito");
+        alert("Taza con Dibujo ha sido comprada");
+        console.log("Taza con Dibujo ha sido comprada");
       }
       console.log(arrayCarrito);
       
       break;
     case 2:
       
-      if (arrayProductos[peluche].stock < 1) {
-        alert("no mas Elefantes no disponible, stocks nuevos cada semana o comunicarse directametne para mayor rapides");  
+      if (arrayProductos[taza].stock < 1) {
+        alert("Se ha agotado el producto de Taza con foto, Si tiene dudas o consultas comuniquese con el centro de atencion al cliente");  
       } else {
         
-        agregarCarrito(arrayProductos[peluche],arrayCarrito);
+        agregarCarrito(arrayProductos[taza],arrayCarrito);
         
-        alert("Lobo agregado al carrito");
-        console.log("Lobo agregado al carrito");
+        alert("Taza con foto ha sido comprada");
+        console.log("Taza con foto ha sido comprada");
       }
       console.log(arrayCarrito);
       
@@ -155,11 +141,11 @@ function calculaCompra(){
 
 function finalizar() {
 
-    console.log("Los productos del carrito son:");
+    console.log("Los productos seleccionados son:");
     for (let index = 0; index < arrayCarrito.length; index++) {
       console.log("-"+arrayCarrito[index].nombre)
     } 
-    alert(`El total de su compra es: $${(calculaCompra().toFixed(2))}`);  
+    alert(`Debera pagar un total de: $${(calculaCompra().toFixed(2))}`);  
 }
 function reiniciar(){
     
@@ -167,4 +153,3 @@ function reiniciar(){
     console.log("------------------------------");
    
 }
-
